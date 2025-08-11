51aa49c0ee6d818e05af12184be458f9ebcf336b
Takayuki Ueno <mail@kichi2004.jp>
2025-08-11 16:01:45 +0900

    Add program

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.InteropServices;
+using static JitShim;
+
+TinyJit.Demo();
+
+
+internal static partial class JitShim
+{
+    [LibraryImport("libjitshim", EntryPoint = "jit_alloc")]
+    internal static partial IntPtr Alloc(nuint size);
+    
+    [LibraryImport("libjitshim", EntryPoint = "jit_free")]
+    internal static partial int Free(nint ptr);
+    
+    [LibraryImport("libjitshim", EntryPoint = "jit_make_executable")]
+    internal static partial int MakeExecutable(nint ptr, nuint size);
+}
+
+[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+internal delegate int MyFunc();
+
+[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+internal delegate int BinOp(int a, int b);
+
+// 0x0131 C9C0
+public static class TinyJit
+{
+    private static readonly byte[] RetNumber =
+    [
+        0x00, 0x38, 0x99, 0x52,  // MOVZ w0, 0xc9c0
+        0x20, 0x26, 0xA0, 0x72,  // MOVK w0, 0x0131, LSL 16
+        0xC0, 0x03, 0x5F, 0xD6,  // RET
+    ];
+
+    private static readonly byte[] Add =
+    [
+        0x20, 0x00, 0x00, 0x0B, // ADD w0, w1, w0
+        0xC0, 0x03, 0x5F, 0xD6, // RET
+    ];
+
+    public static T CreateFuncFromMachineCode<T>(ReadOnlySpan<byte> machineCode) where T : Delegate
+    {
+        nuint size = (nuint) machineCode.Length;
+        var mem = Alloc(size);
+        if (mem == IntPtr.Zero)
+        {
+            throw new OutOfMemoryException("Failed jit_alloc");
+        }
+
+        try
+        {
+            unsafe
+            {
+                var span = new Span<byte>((void*)mem, (int)size);
+                machineCode.CopyTo(span);
+            }
+
+            if (MakeExecutable(mem, size) != 0)
+                throw new InvalidOperationException("Failed jit_make_executable");
+
+            return Marshal.GetDelegateForFunctionPointer<T>(mem);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+            if (Free(mem) != 0)
+            {
+                Console.WriteLine("Failed jit_free");
+            }
+            throw;
+        }
+    }
+
+    public static void FreeFunction<T>(T func) where T : Delegate
+    {
+        var ptr = Marshal.GetFunctionPointerForDelegate(func);
+        if (Free(ptr) != 0)
+        {
+            Console.WriteLine("Failed jit_free");
+        }
+    }
+
+    public static void Demo()
+    {
+        Console.WriteLine("Starting...");
+        var retNumberFn = CreateFuncFromMachineCode<MyFunc>(RetNumber);
+        Console.WriteLine($"ret_number() = {retNumberFn()}");
+        FreeFunction(retNumberFn);;
+        
+        var addFn = CreateFuncFromMachineCode<BinOp>(Add);
+        Console.WriteLine($"add(1, 2) = {addFn(1, 2)}");
+        Console.WriteLine($"add(314159265, -256) = {addFn(314159265, -256)}");
+        FreeFunction(addFn);
+    }
+}
---
+++ libjitshim.c
@@ -0,0 +1,41 @@
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/errno.h>
+
+void __clear_cache(void *start, void *end);
+
+size_t get_page_size() {
+    return (size_t) sysconf(_SC_PAGESIZE);
+}
+
+void *jit_alloc(const size_t size) {
+    const size_t page_size = get_page_size();
+    const size_t alloc = (size + page_size - 1) & ~(page_size - 1);
+    void *ptr = mmap(NULL, alloc, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (ptr == MAP_FAILED) {
+        return NULL;
+    }
+    return ptr;
+}
+
+int jit_make_executable(void *start, const size_t size) {
+    size_t page_size = get_page_size();
+    size_t len = (size + page_size - 1) & ~(page_size - 1);
+
+    __clear_cache(start, start + len);
+    return mprotect(start, len, PROT_READ | PROT_EXEC);
+}
+
+int jit_free(void *start, const size_t size) {
+    size_t page_size = get_page_size();
+    size_t len = (size + page_size - 1) & ~(page_size - 1);
+    return munmap(start, len);
+}
+
+int get_errno() {
+    return errno;
+}
